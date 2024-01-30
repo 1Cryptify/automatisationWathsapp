a17fd787ea8f0d16898394daf6e21ae948b5d625
1Cryptify <austindouglas562@gmail.com>
2024-01-30 20:57:03 +0100

    ajout des nouvelle vue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Box, Typography } from '@mui/material'
+import {  Box } from '@mui/material'
 import Home from './Home'
 function App() {
   return (
--- src/Msg.jsx
+++ src/Msg.jsx
@@ -1,8 +1,19 @@
+import { Box, Button, Grid, InputBase, Paper, TextField, TextareaAutosize, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Msg() {
   return (
-    <div>
-    </div>
+    <Box sx={{height:'100px',widows:'100%'}}>
+        <Grid container>
+        <Grid item xs={6}>
+        <TextField sx={{marginBottom:5,marginTop:5,width:'100%'}} label="Entrer un contact" variant="outlined" />
+        <Button sx={{width:'100%',top:0,padding:2}} variant='contained'>Ajouter a la liste</Button>
+        <Box sx={{borderWidth:1,borderColor:'#2196f3',height:'500px',borderStyle:'solid',
+    borderBottomRightRadius:'100px'}}>
+        </Box>
+        </Grid>
+        <Grid item xs={6}></Grid>
+        </Grid>
+    </Box>
   )
 }
