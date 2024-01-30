bbfb479b6248301605404b11a8f0099755ad5e51
1Cryptify <austindouglas562@gmail.com>
2024-01-30 19:27:25 +0100

    ajout des nouvelle vue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { AppBar, Box, Typography } from '@mui/material'
+import Home from './Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box sx={{display:'flex',flexDirection:'row',flex:1,height:'100vh',justifyContent:'center'}}>
+      <Box sx={{background:'#ececec',width:'60%',height:'100%', padding:'60px'}}>
+        <Home/>
+      </Box>
+    </Box>
+  )
 }
 
-export default App;
+export default App
+
