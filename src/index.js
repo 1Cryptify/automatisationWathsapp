f471ff6ba17d6d8d8ece1497cab1e14638eb1de3
1Cryptify <austindouglas562@gmail.com>
2024-01-31 19:19:59 +0100

    modifier background +

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Box, CircularProgress } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export  function Princ() {
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  // Simulez un chargement asynchrone (par exemple, une requête à une API)
+  const simulateAsyncLoad = async () => {
+    // Utilisez setTimeout pour simuler un chargement pendant 3 secondes (ajustez selon vos besoins)
+    await new Promise(resolve => setTimeout(resolve, 200));
+    
+    // Mettez à jour l'état pour indiquer que le chargement est terminé
+    setLoading(false);
+  };
+
+  simulateAsyncLoad();
+}, []); // Le tableau de dépendances est vide, donc cela s'exécute une seule fois au montage
+
+
+  return (
+    <>
+    {loading?<Box sx={{display:'grid',height:'100vh',placeItems:'center'}}><CircularProgress/></Box>:<App/>}
+    </>
+  )
+}
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Princ/>
   </React.StrictMode>
 );
 
