dac6e627424ea4e734f8cb4b909326d023625141
1Cryptify <austindouglas562@gmail.com>
2024-01-31 16:47:27 +0100

    modifier background +

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import Home from './Home'
 function App() {
   return (
     <Box sx={{
-      backgroundImage: `url('background.jpg')`, // Assurez-vous de spécifier le chemin correct vers votre image
-      backgroundSize: 'cover', // Ajustez la taille de l'image selon vos besoins
-      backgroundPosition: 'center', // Centrez l'image
-      backgroundRepeat: 'no-repeat', // Ne répétez pas l'image
-
       minHeight: '100vh', // Assurez-vous que le conteneur occupe au moins toute la hauteur de la vue
       color: 'black', // Ajoutez une couleur de texte appropriée pour améliorer la lisibilité
     
