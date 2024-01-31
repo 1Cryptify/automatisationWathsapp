475400f29bbb90eedd6d25bdc3fe63ac3ee8f3de
1Cryptify <austindouglas562@gmail.com>
2024-01-31 17:51:54 +0100

    modifier background +

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import {  Box } from '@mui/material'
 import Home from './Home'
+import { styled } from '@mui/material/styles';
+
+const MyApp = styled('div')(({ theme }) => ({
+  height:'100%',
+    [theme.breakpoints.down('md')]: {
+    }
+    ,
+    [theme.breakpoints.up('md')]: {
+      paddingLeft:'10px',
+      paddingRight:'10px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding:'15%',
+      paddingTop:'30px',
+      paddingBottom:'30px'
+    },
+  }));
+
+
+
 function App() {
   return (
     <Box sx={{
       minHeight: '100vh', // Assurez-vous que le conteneur occupe au moins toute la hauteur de la vue
       color: 'black', // Ajoutez une couleur de texte appropriée pour améliorer la lisibilité
     }}>
-      <Box sx={{height:'100%',xs:{background:'red'},md:{background:'blue'}}}>
-        <Home/>
-      </Box>
+      <MyApp><Home/></MyApp>
+        
     </Box>
   )
 }
